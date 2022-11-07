7787e8e206f12125a5d5f9fad9cf377677ddb2bf
diogobabo <diogo.babo@gmail.com>
2022-11-07 16:14:19 +0000

    nsei o que andei a fazer mas acho que assim

--- app/Models/User.php
+++ app/Models/User.php
@@ -8,7 +8,8 @@ use Illuminate\Foundation\Auth\User as Authenticatable;
 class User extends Authenticatable
 {
     use Notifiable;
-
+    protected $table = 'Client';
+    protected $primaryKey = 'idClient';
     // Don't add create and update timestamps in database.
     public $timestamps  = false;
 
@@ -18,7 +19,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'username','password', 'email','firstName','lastName', 'address','phoneNumber',
     ];
 
     /**
@@ -27,7 +28,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $hidden = [
-        'password', 'remember_token',
+        'password',
     ];
 
     /**
