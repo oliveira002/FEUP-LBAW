a16c5e0d833aa4ea55e6fabba054beb6daaab4b3
oliveira <deadzin112@gmail.com>
2022-11-14 22:16:05 +0000

    home

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,20 +8,22 @@
                 <p class = "fw-bold fs-2"> Categories </p>
             </div>
         </div>
-        <div class="row justify-content-md-center mb-5">
-            @foreach($categories as $cat)
-                <div class="col-md-2  mb-4">
-                    <div class = "item">
-                        <img src= "alo.jpg" width="200" height="150">
-                    </div>
+            @foreach($categories->chunk(3) as $chunk)
+                <div class="row justify-content-md-center mb-4">
+                    @foreach($chunk as $add)
+                        <div class="col-md-2">
+                            <div class = "item">
+                                <img src= "alo.jpg" width="200" height="150">
+                            </div>
+                        </div>
+                    @endforeach
                 </div>
             @endforeach
-        </div>
     </div>
         <div class="cover2">
             <div class="row justify-content-md-center ">
                 <div class = "col col-md-6">
-                    <p class = "fw-bold fs-2"> Soon ending! </p>
+                    <p class = "fw-bold fs-2"> Ending Soon! </p>
                 </div>
             </div>
             <div class="row justify-content-md-center mb-5">
