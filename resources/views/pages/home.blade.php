dccb62212430d8071459890803c9534dbec3c4c6
oliveira <deadzin112@gmail.com>
2022-11-15 12:16:59 +0000

    responsive grid

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,16 +2,14 @@
 
 
 @section('content')
-    <div class="cover">
-        <div class="row justify-content-md-center">
-            <div class = "col-md-6">
-                <p class = "fw-bold fs-2"> Categories </p>
-            </div>
+    <div class="container-fluid">
+        <div class="d-flex flex-row flex-wrap justify-content-center ">
+            <p class = "fw-bold fs-2"> Categories </p>
         </div>
             @foreach($categories->chunk(3) as $chunk)
-                <div class="row  justify-content-md-center mb-5">
+                <div class="d-flex flex-row flex-wrap justify-content-center ">
                     @foreach($chunk as $add)
-                        <div class="col-md-2" style = "min-width: 300px; max-width: 300px">
+                        <div class="d-flex flex-column p-3">
                             <div class = "item">
                                 <img src= "alo.jpg" width="287" height="190">
                             </div>
@@ -20,25 +18,24 @@
                 </div>
             @endforeach
     </div>
-        <div class="cover2">
-            <div class="row justify-content-md-center">
-                <div class = "col-md-6">
-                    <p class = "fw-bold fs-2"> Ending Soon! </p>
-                </div>
-            </div>
-            <div class="row justify-content-md-center mb-5 p-0">
-                @foreach($auctions as $auct)
-                    <div class="col-md-2">
-                        <div class = "item">
-                            <img src= "alo.jpg" width="287" height="190">
-                            <div class = "prop">
-                                <p class = "fw-bold"> {{$auct->name}} </p>
-                                <p class = "fw-bold"> {{$auct->currentprice}}$ </p>
-                                <p class = "fw-bold"> {{$auct->enddate}} </p>
-                            </div>
+    <div class="container-fluid">
+        <div class="d-flex flex-row flex-wrap justify-content-center ">
+            <p class = "fw-bold fs-2 mb-0"> Ending Soon! </p>
+        </div>
+        <div class="d-flex flex-row flex-wrap justify-content-center ">
+            @foreach($auctions as $auct)
+                <div class="d-flex flex-column p-3">
+                    <div class = "item">
+                        <img src= "alo.jpg" width="287" height="190">
+                        <div class = "prop">
+                            <p class = "fw-bold "> {{$auct->name}} </p>
+                            <p class = "fw-bold"> {{$auct->currentprice}}$ </p>
+                            <p class = "fw-bold"> {{$auct->enddate}} </p>
                         </div>
                     </div>
-                @endforeach
-            </div>
+                </div>
+            @endforeach
         </div>
+    </div>
+
 @endsection
