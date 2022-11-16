a8648d1215cc31793fac5eff800112bfd7e3e203
oliveira <deadzin112@gmail.com>
2022-11-16 16:31:56 +0000

    auction page

--- resources/views/pages/auction.blade.php
+++ resources/views/pages/auction.blade.php
@@ -12,6 +12,7 @@
         $hour = "0" . $hour;
     }
     $finalStr = $monthstr.  " " .  $day. "," . " "  .$year . " " . $hour.":".$mins.":".$secs;
+    $minBid = 1.05 * $auction->startingprice;
 ?>
 
 
@@ -19,11 +20,53 @@
 @section('content')
         <div class = "page">
             <div class = "d-flex">
-                <div class="foto">
-                    <img src = "item.jpg" width= "400" height = "510">
+                <div>
+                    <div class="foto">
+                        <img src = "../item.jpg" width= "400" height = "510">
+                    </div>
+                    <div class = "mt-2 hist">
+                        <p class = "h2 fw-bold mb-0"> Current Bid: </p>
+                        <p class = "pp fw-bold mb-0"> {{$auction->currentprice}}€</p>
+                    </div>
+                    <div class = "text-center">
+                        <p class = "h4 fw-bold mb-2"> View Bidding History: </p>
+                        <p> Bidder 4 - 28th September   1600€</p>
+                        <p> Bidder 3 - 28th September   1600€</p>
+                        <p> Bidder 2 - 28th September   1600€</p>
+                        <p> Bidder 1 - 28th September   1600€</p>
+
+                    </div>
                 </div>
-                <div class="infos mt-4 ms-4">
-                    <p class = "fw-bold h3"> {{$auction->name}}</p>
+                <div class="infos ms-4">
+                    <p class = "details fw-bold h5 mb-0"> Product Details: </p>
+                    <div class="texto">
+                        <p class = "fw-bold">{{$auction->description}}</p>
+                    </div>
+                </div>
+                <div class="bids ms-4">
+                    <p class = "details fw-bold h5 mb-0"> Name: </p>
+                    <div>
+                        <p class = "fw-bold"> {{$auction->name}} </p>
+                    </div>
+                    <p class = "details fw-bold h5 mb-0"> Initial Price: </p>
+                    <div>
+                        <p class = "fw-bold">{{$auction->startingprice}}€</p>
+                    </div>
+                    <p class = "details fw-bold h5 mb-0"> Auction Ends In: </p>
+                    <div>
+                        <p class = "fw-bold">24h</p>
+                    </div>
+                    <p class = "details fw-bold h5"> Bid Price (€): </p>
+                    <div class="acts">
+                        <input type="number" class="inpt ps-1 fw-bold" name = "name" value = "{{$minBid}}" min= {{$minBid}}>
+                        <div class = "mt-4">
+                            <a class = "bidbtn text-center" href = "">
+                                <button class = "fw-bold">
+                                     Place Bid 
+                                </button>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
