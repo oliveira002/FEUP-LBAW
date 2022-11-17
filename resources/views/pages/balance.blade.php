a06c78e27fd072330eccc7af34a18cde2b656abd
oliveira <deadzin112@gmail.com>
2022-11-17 14:00:38 +0000

    home

--- resources/views/pages/balance.blade.php
+++ resources/views/pages/balance.blade.php
@@ -4,70 +4,7 @@
     <link href="{{asset('css/profile.css')}}" rel="stylesheet">
     <div class = "cover out3">
         <div class="prof d-flex">
-            <div id="aside">
-                <div class="hi d-flex pt-4 pb-4">
-                    <div class="lg">
-                        <img src= "/alo.jpg" width="120" height="120">
-                    </div>
-                    <div class="nome ms-2">
-                        <p class = "fw-bold mb-1">Hi,</p>
-                        <p class = "fw-bold mb-0"> {{$user->firstname}} {{$user->lastname}} </p>
-
-                    </div>
-                </div>
-                <ul class = "ps-0 mt-2">
-                    <li>
-                        <a href=""><button class = "fw-bold">
-                                <i class="fa-solid fa-user"></i>
-                                Account Overview
-                            </button> </a>
-                    </li>
-                    <li>
-                        <a href=""><button class = "fw-bold">
-                                <i class="fa-solid fa-address-card"></i>
-                                My Details
-                            </button>
-                        </a>
-                    </li>
-                    <li>
-                        <a href=""><button class = "fw-bold">
-                                <i class="fa-solid fa-wallet"></i>
-                                My Wallet
-                            </button>
-                        </a>
-                    </li>
-                    <li>
-                        <a href=""><button class = "fw-bold">
-                                <i class="fa-solid fa-coins"></i>
-                                My Bids</button>
-                        </a>
-                    </li>
-                    <li>
-                        <a href=""><button class = "fw-bold">
-                                <i class="fa-solid fa-house-user"></i>
-                                My Auctions</button>
-                        </a>
-                    </li>
-                    <li>
-                        <a href=""><button class = "fw-bold">
-                                <i class="fa-solid fa-star"></i>
-                                Favourites</button>
-                        </a>
-                    </li>
-                    <li>
-                        <a href=""><button class = "fw-bold">
-                                <i class="fa-solid fa-question"></i>
-                                Support</button>
-                        </a>
-                    </li>
-                    <li>
-                        <a href=""><button class = "fw-bold">
-                                <i class="fa-solid fa-right-from-bracket"></i>
-                                Logout</button>
-                        </a>
-                    </li>
-                </ul>
-            </div>
+            @include('partials.sidebar')
             <div>
                 <div class="spec d-flex flex-column ps-3 pe-3" id="balance-container">
                     <div class = "stuf ms-3 mt-5 mb-4">
