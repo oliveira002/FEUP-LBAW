4f9e4aa5447cb617b5682fa3f05621c728286db6
diogobabo <diogo.babo@gmail.com>
2022-11-17 14:46:57 +0000

    stuff

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -47,7 +47,7 @@ class UserController extends Controller
      */
     public function show($id)
     {
-        $user = User::find($id);
+        $user = User::findOrFail($id);
         return view('pages.profile',['user' => $user]);
     }
 
