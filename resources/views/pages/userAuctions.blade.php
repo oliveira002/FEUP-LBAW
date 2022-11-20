1d7a81ebb2a19478f8da2ca90eed4c432b4abcfc
up202004187 <up202004187@up.pt>
2022-11-20 17:22:52 +0000

    create auction on profile

--- resources/views/pages/userAuctions.blade.php
+++ resources/views/pages/userAuctions.blade.php
@@ -9,17 +9,19 @@
                 <div class="spec d-flex flex-column ps-3 pe-3" id="user-auctions">
                     <div class="stuf ms-3 mt-4 mb-4">
                         <div class="stuf ms-3 mt-5 mb-4">
-                            <h1 class="fw-bold">My Auctions</h1>
+                            <div class="d-flex">
+                                <h1 class="fw-bold">My Auctions</h1>
+                                <a class="new_auc" href="{{route('createAuction')}}"><i class="fa-solid fa-plus"></i> Create Auction</a>
+                            </div>
                             <p class="fw-bold">Here you can see all your auctions</p>
                             <hr class="mt-3 mb-3">
                             @if(count($auctions)==0)
-                                <div>You have no auctions. Click <a href="{{route('createAuction')}}"><u>here</u></a> to create one
-                                </div>
+                                <div>You have no auctions. Create one! </div>
                             @else
                                 @foreach($auctions as $auct)
                                     <div class="row">
                                         <div class="col-3">
-                                            <img src="/item.jpg" width="130" height="95">
+                                            <img src="../images/{{$auct->idauction}}/1.jpg" width="130" height="95">
                                         </div>
                                         <div class="col-9">
                                             <div class="row">
@@ -33,7 +35,6 @@
                                                     <p class="fw-bold">Ends: {{$auct->enddate}}</p>
                                                 </div>
                                                 <div class="col-12" id="item-info">
-
                                                     <a href="{{route('auction',['id' => $auct->idauction])}}">
                                                         <button class="fw-bold btn btn-secondary btn-sm">View Auction
                                                         </button>
