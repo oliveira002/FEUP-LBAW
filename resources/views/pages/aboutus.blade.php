98f633da533553e890b249a587a77098fd66a54e
up202004187 <up202004187@up.pt>
2022-12-27 03:09:35 +0000

    About us Fix

--- resources/views/pages/aboutus.blade.php
+++ resources/views/pages/aboutus.blade.php
@@ -2,7 +2,7 @@
 
 @section('content')
     <img src="images/aboutus/diogobabo.jpg">
-    <img src="images/aboutus/gustavocosta.jpg">
+    <img src="images/aboutus/gustavocosta.png">
     <img src="images/aboutus/joaooliveira.jpg">
     <img src="images/aboutus/ricardocavalheiro.jpg">
 @endsection
