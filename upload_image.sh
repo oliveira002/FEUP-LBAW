faf44f4c3341e9172abbbf05a82aced328b5c80f
diogobabo <diogo.babo@gmail.com>
2023-01-03 16:35:56 +0000

    eviorement

--- upload_image.sh
+++ upload_image.sh
@@ -7,8 +7,10 @@ IMAGE_NAME=git.fe.up.pt:5050/lbaw/lbaw2122/lbaw2225 # Replace with your group's 
 
 # Ensure that dependencies are available
 composer install
+php artisan config:clear
 php artisan clear-compiled
 php artisan optimize
+
 
 docker build -t $IMAGE_NAME .
 docker push $IMAGE_NAME
