99b15d06d5e9b231052e5239540bbcffa26c52b5
RicardoCavalheiro123 <up202005103@fe.up.pt>
2023-01-03 19:25:08 +0000

    ban appeal

--- app/Http/Controllers/BanAppealController.php
+++ app/Http/Controllers/BanAppealController.php
@@ -3,17 +3,28 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Support\Facades\Auth;
+use App\Models\BanAppeals;
 
 class BanAppealController extends Controller
 {
     public function banappeal(){
 
         if(Auth::user()){
-            //get all ban appeals
-            /*$ban_appeals = BanAppeal::selectRaw('*')
+            //check if user is banned
+            $user = Auth::user();
+            if($user->isbanned)
+            {
+                $ban_appeals = BanAppeals::selectRaw('*')
                 ->where('idclient','=',Auth::user()->idclient)
-                ->get();*/
-            return view('pages.banappeal', []);
+                ->get();
+                return view('pages.banappeal', ['ban_appeals' => $ban_appeals]);
+
+            }
+            else
+            {
+
+                return redirect()->route('/');
+            }
 
         }
         else{
--- resources/views/pages/banappeal.blade.php
+++ resources/views/pages/banappeal.blade.php
@@ -2,7 +2,8 @@
 
 @section('content')
     <div class="contact">
-        @if(false)
+
+        @if(count($ban_appeals)==0)
         <h3 class="fw-bold mb-5 mt-5">You are Banned!</h3>
         <form action="">
               <span class="fw-bold">You are unable to browse the website, as well as create auctions and place bids!</span>
@@ -16,8 +17,14 @@
             </div>
         </form>
         @else
+
             <h3 class="fw-bold mb-5 mt-5">You have already requested an unban</h3>
-            //show your appeal
+            <h3 class="request mt-5">Your Unban Request:</h3>
+            <span class="bandesc mb-3">{{$ban_appeals->first()->appealdescription}}</span>
+            <div class="d-flex justify-content-center">
+                <span class="fw-bold pe-1">Date:</span>
+                <span class="date">{{$ban_appeals->first()->appealdate}}</span>
+            </div>
 
             <span class="fw-bold">Waiting for a response from admin</span>
             <div class="loading">
